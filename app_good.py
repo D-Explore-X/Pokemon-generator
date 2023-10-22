cd359a70659af418f856ae999c8ec25648201326
Zeshan Ali <zishraj07@gmail.com>
2023-10-22 18:28:23 +0500

    updated

--- app_good.py
+++ app_good.py
@@ -2,11 +2,13 @@ from flask import Flask, render_template, request, jsonify
 import json
 import random
 
+import os
+
 app = Flask(__name__)
 
 # Load Pokémon data from JSON files
 
-data_dir = "data/"
+data_dir = "data_updated/"
 regions = [
     "all.json",
     "alola_usum.json",
@@ -47,7 +49,9 @@ current_options = DEFAULT_OPTIONS
 
 @app.route('/')
 def index():
-    return render_template('index.html', options=current_options)
+    return render_template('indexxx.html', options=current_options)
+
+# ...
 
 @app.route('/generate', methods=['POST'])
 def generate_pokemon():
@@ -61,8 +65,15 @@ def generate_pokemon():
     # Filter Pokémon based on options
     eligible_pokemon = []
     for pokemon in selected_region:
-        if (current_options["legendaries"] or not pokemon["isLegendary"]) \
-            and (current_options["nfes"] or not pokemon["isNfe"]):
+        # Check if the 'types' key exists in the pokemon dictionary
+        if (
+            (current_options["legendaries"] or not pokemon.get("isLegendary", False)) and
+            (current_options["nfes"] or not pokemon.get("isNfe", False)) and
+            (
+                current_options["type"] == "all" or
+                ("types" in pokemon and current_options["type"] in pokemon["types"])
+            )
+        ):
             eligible_pokemon.append(pokemon)
 
     # Randomly select N Pokémon
@@ -71,7 +82,48 @@ def generate_pokemon():
         num_pokemon = len(eligible_pokemon)
     selected_pokemon = random.sample(eligible_pokemon, num_pokemon)
 
-    return jsonify(selected_pokemon)
+    # Get sprite URLs for selected Pokémon
+    sprite_urls = []
+    if current_options["sprites"]:
+        sprite_urls = [get_sprite_url(pokemon) for pokemon in selected_pokemon]
+
+    # Prepare data to send to the client
+    response_data = {
+        "pokemon": selected_pokemon,
+        "sprite_urls": sprite_urls,
+    }
+
+    # If "forms" option is selected, include forms data in the response
+    if current_options["forms"]:
+        forms = []
+        for pokemon in selected_pokemon:
+            if "forms" in pokemon:
+                forms.extend(pokemon["forms"])
+        response_data["forms"] = forms
+    for pokemon in selected_pokemon:
+        if "desc" in pokemon:
+            response_data.setdefault("desc", []).append(pokemon["desc"])
+
+    return jsonify(response_data)
+def get_sprite_url(pokemon):
+    sprite_dir = "static/pokemon_sprites"
+    sprite_type = "normal"
+    sprite_suffix = ""
+    
+    if current_options["sprites"]:
+        sprite_type = "shiny" if "shiny" in current_options else "normal"
+    
+    # Construct the sprite file name based on Pokémon name
+    sprite_filename = f"{pokemon['name'].lower()}{sprite_suffix}.png"
+    
+    # Define the sprite path relative to the "static" folder
+    sprite_path = os.path.join(sprite_dir, sprite_type, sprite_filename)
+    
+    # Construct the URL using Flask's send_from_directory
+    sprite_url = sprite_path
+    
+    return sprite_url
 
 if __name__ == '__main__':
+    app.config['SITE_URL'] = "http://localhost:5000"
     app.run(debug=True)
